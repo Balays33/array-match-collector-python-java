904299ba4f85f10b537b5f06b342183af22a55d8
Ubuntu <ubuntu@ip-172-31-6-126.ec2.internal>
2021-03-28 14:16:27 +0000

    update

--- prog-Array.py
+++ prog-Array.py
@@ -2,24 +2,47 @@ from random import randrange
 import random
 import string
 
+myarray = []
 
 class Progress:
-    def __init__(self, sizeofarray,name):
+    def __init__(self, sizeofarray,name,myarray):
         self.sizeofarray = sizeofarray
         self.name = name
-        myarray = []
+        self.myarray = myarray
+        caraterNumber = 3
+
+    def createArray1(self):
+        i=0
+        #print(self.sizeofarray)
+        
+        if int(self.sizeofarray)<50:
+            self.sizeofarray =50
+            print(self.sizeofarray)
+            
+        while i<self.sizeofarray:
+            #print(i)
+            mystring =""
+            for x in range(3):
+                #print(x)
+                randomCH = chr(random.randint(ord('a'), ord('z'))) 
+                mystring += randomCH
+            myarray.append(mystring)
+            i+=1
+               
+          
+        return(myarray)
+        
+    def welcome(self):
+        print("Hello {}".format(name))
+        
+    
 
 
-#name = str(input("What is your name : "))
-#sizeofarray = input("Size of the array : ")
+name = str(input("What is your name : "))
+sizeofarray = input("Size of the array : ")
 
-#print(name)
-#p1 = Progress(sizeofarray,name)
 
-value = input("Please enter a string:\n")
- 
-print(f'You entered {value} and its type is {type(value)}')
- 
-value = input("Please enter an integer:\n")
- 
-print(f'You entered {value} and its type is {type(value)}')
+p1 = Progress(sizeofarray,name,myarray)
+
+p1.welcome()
+p1.createArray1()
