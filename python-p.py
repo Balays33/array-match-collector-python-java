4c5298f507e24330b512d31dfd673be2a5e72810
Balazs Barcza <40806995+Balays33@users.noreply.github.com>
2021-03-29 22:28:51 +0100

    Update python-p.py

--- python-p.py
+++ python-p.py
@@ -1,49 +1,104 @@
-print('This is a pytone progarm')
-print('array-match-collector-python')
-
-
-
 from random import randrange
 import random
 import string
-print(randrange(10))
-
-     
-# Randomly choose a letter from all the ascii_letters 
-randomLetter = random.choice(string.ascii_letters) 
-print(randomLetter)
-
-# Randomly generate a ascii value 
-# from 'a' to 'z' and 'A' to 'Z' 
-randomLowerLetter = chr(random.randint(ord('a'), ord('z'))) 
-randomUpperLetter = chr(random.randint(ord('A'), ord('Z'))) 
-print(randomLowerLetter, randomUpperLetter)
-
 
 myarray = []
-inputNumber = input("Size of the array : ")
-i = 0
-# size of the array
-if inputNumber<50:
-    inputNumber =100
-  
-while i<inputNumber:
-    #print(i)
-    mystring =""
-    for x in range(3):
-        #print(x)
-        randomCH = chr(random.randint(ord('a'), ord('z'))) 
-        mystring += randomCH
-    myarray.append(mystring)
-    i+=1
-    
-print(myarray)
+myarray2 = []
+myarray3 = []
 
 class Progress:
-    def __init__(self, name, age):
+    def __init__(self, sizeofarray,name,myarray,myarray2,myarray3):
+        self.sizeofarray = sizeofarray
         self.name = name
-        self.age = age
+        self.myarray = myarray
+        self.myarray2 = myarray2
+        self.myarray3 = myarray3
+        caraterNumber = 3
 
-p1 = Progress("John",36)
+    def createArray1(self):
+        i=0
+        #print(self.sizeofarray)
+        
+        if int(self.sizeofarray)<50:
+            self.sizeofarray =50
+            #print(self.sizeofarray)
+        elif int(self.sizeofarray)>100:
+            self.sizeofarray =1000
+        else:
+            print("OK progress")
+        
+        
+        while i<int(self.sizeofarray):
+            #print(i)
+            mystring =""
+            for x in range(3):
+                #print(x)
+                randomCH = chr(random.randint(ord('a'), ord('z'))) 
+                mystring += randomCH
+            myarray.append(mystring)
+            i+=1
+        i=0
+        while i<int(self.sizeofarray):
+            #print(i)
+            mystring =""
+            for x in range(3):
+                #print(x)
+                randomCH = chr(random.randint(ord('a'), ord('z'))) 
+                mystring += randomCH
+            myarray2.append(mystring)
+            i+=1
+        i=0
+        while i<int(self.sizeofarray):
+            #print(i)
+            mystring =""
+            for x in range(3):
+                #print(x)
+                randomCH = chr(random.randint(ord('a'), ord('z'))) 
+                mystring += randomCH
+            myarray3.append(mystring)
+            i+=1
+        return(myarray,myarray2,myarray3)
+        
+    def welcome(self):
+        print("Hello {}".format(name))
+        
+    def sortoutArray(self,myarray):
+        #print(myarray)
+        myarray.sort()
+        #print(myarray)
+        return myarray
+        
+    def findmatch(self,myarray,myarray2,myarray3):
+        howmanyMatch =0
+        #print (len(myarray))
+        for x in range(len(myarray)-1):
+            for y in range(len(myarray)):
+                if myarray[x]==myarray2[y]:
+                    for z in range(len(myarray)):
+                        if myarray2[y]==myarray3[z]:
+                            howmanyMatch += 1
+                            print("this is the position first array {} and this is the code {} ".format(x,myarray[x]))
+                            print("this is the position second array {} and this is the code {} ".format(y,myarray2[y]))
+                            print("this is the position third array {} and this is the code {} ".format(z,myarray3[z]))
+                                   
+        return howmanyMatch
+        
 
-print(p1.name)
+
+name = str(input("What is your name : "))
+sizeofarray = input("Size of the array : ")
+
+
+p1 = Progress(sizeofarray,name,myarray,myarray2,myarray3)
+
+p1.welcome()
+p1.createArray1()
+#print("This is the first before sort array{}".format(myarray))
+#print("This is the second before sort array{}".format(myarray2))
+p1.sortoutArray(myarray)
+p1.sortoutArray(myarray2)
+p1.sortoutArray(myarray3)
+#print("This is the first array{}".format(myarray))
+#print("This is the second array{}".format(myarray2))
+#print("This is the third array{}".format(myarray3))
+p1.findmatch(myarray,myarray2,myarray3)
